4c929e429df185d83d7f5b505144f2ecc3c1bf42
Mohammad Amir <amir0028m@gmail.com>
2022-04-13 18:34:42 +0430

    edit my code

--- app.py
+++ app.py
@@ -2,10 +2,8 @@ a = int(input("enter number:"))
 count = 0
 sum = a
 while a != -1:
-    print(a , sum)
-    count = count + 1
-    sum = sum + a
-    #a = a-1
+    sum += sum
+    count += 1
     a = int(input("enter number:"))
 
-    print("tedad:", count, "jam:", sum, "miangin:", sum / count)
+print(f"Average : {sum / count} | sum : {sum} | count : {count}")
